c81a6744e956b831b6d52933f286f8d8c4a84567
yijie.yan <yijie.yan@outlook.com>
2018-04-10 10:48:22 +0800

    init add hotcss..js reset.css config scss

---
+++ src/pages/home.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div :class="$style.home">
+    hello world
+    <p>i am child</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" module>
+  .home {
+    color:red;
+    font-size: 100px;
+    p {
+      color: blue;
+    }
+  }
+</style>
