4f2b5f62a64d04eb0dc8353f7f652388be5564f2
yijie.yan <yijie.yan@outlook.com>
2018-04-27 17:06:51 +0800

    end

--- src/pages/home/components/HomeSwiper.vue
+++ src/pages/home/components/HomeSwiper.vue
@@ -30,7 +30,7 @@ export default {
   data () {
     return {
       swiperOption: {
-        // autoplay: true,
+        autoplay: true,
         loop: true,
         pagination: {
           el: '.swiper-pagination'
